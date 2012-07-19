c66f0475a3a2bb7a2b560d1098f8c478e4f8cc9f
Matteo Depalo <matteodepalo@gmail.com>
2012-07-19 16:19:06 +0200

    Updated to rails 3.2.6

--- lib/chameleon.rb
+++ lib/chameleon.rb
@@ -1,14 +1,4 @@
-module Chameleon
-  class Engine < Rails::Engine
-    isolate_namespace Chameleon
-    initializer "chameleon.initialization" do
-      Dir.glob(File.join("app", "widgets", "*.rb")).each { |f| require File.expand_path(f) }
-    end
-  end
-end
+require 'chameleon/engine'
 
-Kernel.class_eval do
-  def widget(name, &block)
-    Chameleon::Widget.widget(name, &block)
-  end
+module Chameleon
 end
