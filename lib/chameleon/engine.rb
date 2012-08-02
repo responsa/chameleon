eae6d5ed30cd9706855c1bbed3ee8fb8597ca4e0
Matteo Depalo <matteodepalo@gmail.com>
2012-08-02 15:31:54 +0200

    Initializer in engine was deleted in favour of prepare

--- lib/chameleon/engine.rb
+++ lib/chameleon/engine.rb
@@ -1,7 +1,7 @@
 module Chameleon
   class Engine < Rails::Engine
     isolate_namespace Chameleon
-    initializer "chameleon.initialization" do
+    config.to_prepare do
       Dir.glob(File.join("app", "widgets", "*.rb")).each { |f| require_dependency File.expand_path(f) }
     end
   end
