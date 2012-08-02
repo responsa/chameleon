4ff9690c1e14896bf12e6c1c0fd706cdf772a3c2
Matteo Depalo <matteodepalo@gmail.com>
2012-08-02 12:55:11 +0200

    Changed require with require_dependency and bumped to 0.2.6

--- lib/chameleon/engine.rb
+++ lib/chameleon/engine.rb
@@ -2,7 +2,7 @@ module Chameleon
   class Engine < Rails::Engine
     isolate_namespace Chameleon
     initializer "chameleon.initialization" do
-      Dir.glob(File.join("app", "widgets", "*.rb")).each { |f| require File.expand_path(f) }
+      Dir.glob(File.join("app", "widgets", "*.rb")).each { |f| require_dependency File.expand_path(f) }
     end
   end
 end
