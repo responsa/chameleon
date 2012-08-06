8c4dddecf472e69585a5c10b5565b1fcc2826015
Matteo Depalo <matteodepalo@gmail.com>
2012-08-06 18:39:45 +0200

    Changed config.prepare to initializer

--- lib/chameleon/engine.rb
+++ lib/chameleon/engine.rb
@@ -1,7 +1,7 @@
 module Chameleon
   class Engine < Rails::Engine
     isolate_namespace Chameleon
-    config.to_prepare do
+    initializer 'chameleon' do |app|
       Dir.glob(File.join("app", "widgets", "*.rb")).each { |f| require_dependency File.expand_path(f) }
     end
   end
